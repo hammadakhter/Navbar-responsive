02fb0d22f050b1285f6e154eacb176b9598cd8d1
M.Hammad <hammmadakhter@gmail.com>
2024-08-19 15:36:37 +0500

    few changes in navbar
    few changes in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,25 +1,43 @@
 import "./navbar.css"
-import React from 'react'
+import React, { useState } from 'react';
 
 function Navbar() {
- 
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleMenuItemClick = () => {
+    if (isMenuOpen) {
+      setIsMenuOpen(false);
+    }
+  };
+
   return (
     <div className='container'>
-        <nav>
-          <input type="checkbox" id="check"/>
-          <label htmlFor="check" className="checkbtn">
-          <i className="fas fa-bars"></i>
-          </label>
-          <label className="logo">portfolio</label>
-          <ul>
-            <li><a className="active" href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="#">Feedback</a></li>
-          </ul>
-        </nav>
-    </div>
+    <nav>
+      <input
+        type="checkbox"
+        id="check"
+        checked={isMenuOpen}
+        onChange={handleMenuToggle}
+        onClick={handleMenuToggle}
+      />
+      <label htmlFor="check" className="checkbtn" >
+        <i className="fas fa-bars"></i>
+      </label>
+      <label className="logo">portfolio</label>
+      <ul className={isMenuOpen ? 'active' : ''}>
+        <li><a href="#" onClick={handleMenuItemClick}>Home</a></li>
+        <li><a href="#" onClick={handleMenuItemClick}>About</a></li>
+        <li><a href="#" onClick={handleMenuItemClick}>Services</a></li>
+        <li><a href="#" onClick={handleMenuItemClick}>Contact</a></li>
+        <li><a href="#" onClick={handleMenuItemClick}>Feedback</a></li>
+      </ul>
+    </nav>
+  </div>
   )
 }
 
